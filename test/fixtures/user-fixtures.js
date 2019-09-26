38690bfb74be3198ccff79eac613854c9ffd477c
fabpulaws <faboulaws@gmail.com>
2019-09-26 23:36:47 +0300

    code cleanup and formatting

--- test/fixtures/user-fixtures.js
+++ test/fixtures/user-fixtures.js
@@ -1,12 +1,12 @@
 const Fixtures = require('node-mongodb-fixtures');
 module.exports = async () => {
-    const fixtures = new Fixtures({dir: 'test/fixtures/entities'});
+    const fixtures = new Fixtures({ dir: 'test/fixtures/entities' });
     await fixtures.connect(process.env.DB_URI).then(() => fixtures.load());
     const users = await Promise.resolve(fixtures._db.collection('users'))
-       .then((collection) => {
-         return collection.find().toArray();
-       }); 
+        .then((collection) => {
+            return collection.find().toArray();
+        });
     const unseed = () => fixtures.unload()
         .then(() => fixtures.disconnect());
-    return {unseed, users};
+    return { unseed, users };
 }
--- test/user-repository-tests.js
+++ test/user-repository-tests.js
@@ -2,14 +2,14 @@ const { expect } = require('chai');
 const seedUsers = require('./fixtures/user-fixtures');
 const { fetchUserByName } = require('../lib/repositories/user-repository');
 describe('user fetching logic', () => {
-let unseed, users;
+    let unseed, users;
     before(async () => {
-        ({unseed, users} = await seedUsers())
+        ({ unseed, users } = await seedUsers())
     });
     after(() => unseed());
     it('must fetch a user by ID', async () => {
         const [userOne] = users;
-        const  actualUser = await fetchUserByName(userOne.username);
+        const actualUser = await fetchUserByName(userOne.username);
         expect(actualUser).to.eql(userOne);
     });
 });

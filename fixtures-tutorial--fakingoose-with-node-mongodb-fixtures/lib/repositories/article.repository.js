b87b3bbaba7e7cb22e33d5990eb6025817cf9f72
fabpulaws <faboulaws@gmail.com>
2019-10-04 00:37:14 +0300

    article repo code cleanup

--- fixtures-tutorial--fakingoose-with-node-mongodb-fixtures/lib/repositories/article.repository.js
+++ fixtures-tutorial--fakingoose-with-node-mongodb-fixtures/lib/repositories/article.repository.js
@@ -4,8 +4,6 @@ const ArticleModel = mongoose.model('article', ArticleSchema);
 
 exports.getAll = async () => {
     try {
-        // await mongoose.connect(process.env.DB_URI, { useNewUrlParser: true });
-
         const articles = await ArticleModel.find({}).lean();
         return articles;
     } catch (e) {
